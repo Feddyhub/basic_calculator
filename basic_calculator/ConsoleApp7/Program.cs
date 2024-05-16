b7dd3ea04b361fd756cd4a9fc96a35a74abf3fcb
Murat Ferhat Derya <91869273+Feddyhub@users.noreply.github.com>
2024-05-16 22:53:20 +0300

    Add files via upload

---
+++ basic_calculator/ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿
+   double a, b,sonuc=0;
+    char islem;
+
+    a = Convert.ToInt64(Console.ReadLine());
+    b = Convert.ToInt64 (Console.ReadLine());
+    islem = Convert.ToChar(Console.ReadLine());
+
+    switch(islem)
+    { 
+        case '+':
+            sonuc = a + b;
+            break;
+        case '-':
+            sonuc = a - b;
+            break;
+        case '*':
+            sonuc = a * b;
+            break;
+        case '/':
+            sonuc = a / b;
+            break;
+        default:
+            Console.WriteLine("HATA");
+            return;
+    }
+    Console.WriteLine(sonuc);
